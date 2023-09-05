cc3fb69d64a9a49ddd18367685efb3347d71ab4b
Denis Bogatirov <denis.bohatyrov@computools.com>
2023-09-05 12:40:32 +0300

    Fix example, update pubspec

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,14 +2,36 @@ import 'package:flutter/material.dart';
 
 import 'package:circle_wheel_scroll/circle_wheel_scroll_view.dart';
 
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: WheelExample(),
+        ),
+      ),
+    );
+  }
+}
+
 class WheelExample extends StatelessWidget {
+  const WheelExample({ super.key });
+
+
   Widget _buildItem(int i) {
     return Center(
       child: ClipRRect(
         borderRadius: BorderRadius.circular(40),
         child: Container(
           width: 80,
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           color: Colors.blue[100 * ((i % 8) + 1)],
           child: Center(
             child: Text(
@@ -25,7 +47,7 @@ class WheelExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wheel example'),
+        title: const Text('Wheel example'),
       ),
       body: SafeArea(
         child: Center(
@@ -33,13 +55,13 @@ class WheelExample extends StatelessWidget {
             // height: 260,
             // width: 160,
             child: CircleListScrollView(
-              physics: CircleFixedExtentScrollPhysics(),
+              physics: const CircleFixedExtentScrollPhysics(),
               axis: Axis.vertical,
               itemExtent: 80,
-              children: List.generate(20, _buildItem),
               radius: MediaQuery.of(context).size.width * 0.6,
               onSelectedItemChanged: (int index) =>
                   print('Current index: $index'),
+              children: List.generate(20, _buildItem),
             ),
           ),
         ),
@@ -47,18 +69,3 @@ class WheelExample extends StatelessWidget {
     );
   }
 }
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WheelExample(),
-    );
-  }
-}
-
-void main() => runApp(MyApp());
